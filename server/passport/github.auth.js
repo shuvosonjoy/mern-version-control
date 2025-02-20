6fe642534764e4ae7dcf9f6da38d11084611a725
shuvosonjoy <shuvosdg123@gmail.com>
2025-02-20 16:31:40 +0600

    deploy error

--- server/passport/github.auth.js
+++ server/passport/github.auth.js
@@ -14,11 +14,6 @@ passport.serializeUser(function(user, done) {
   });
   
 
-
-  // Use the GitHubStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and GitHub
-//   profile), and invoke a callback with a user object.
 passport.use(new GitHubStrategy({
 
     clientID: process.env.GITHUB_CLIENT_ID,
